0d57ad3dce5b1218f2cc39bf88414ee22cbc4491
Matheus <matheus.viana28l@gmail.com>
2020-01-09 21:21:17 -0300

    anma

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://anma.herokuapp.com',
+  apiUrl: 'https://anma-api.herokuapp.com',
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
-  //apiUrl: 'https://anma.herokuapp.com'
+  //apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://anma-api.herokuapp.com'
 };
 
 /*
